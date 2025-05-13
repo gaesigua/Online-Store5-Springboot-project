897f6b7978679fa424a345fcaea7169290b2095d
gaesigua <67262987+gaesigua@users.noreply.github.com>
2025-05-13 09:42:19 +0200

    first commit

---
+++ src/test/java/com/gasigwatin/Online_Store5_Springboot_project/OnlineStore5SpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasigwatin.Online_Store5_Springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStore5SpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
